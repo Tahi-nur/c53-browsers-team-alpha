8b070760ef389e86bcd81bb2274ed1c5e4f1b8aa
888i88 <igor_palun@icloud.com>
2025-06-18 12:53:08 +0200

    Initialize app on load only once

--- src/app.js
+++ src/app.js
@@ -2,9 +2,7 @@ import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
 const loadApp = () => {
-  quizData.currentQuestionIndex = 0;
-
-  initWelcomePage();
+    initWelcomePage();
 };
 
-window.addEventListener('load', loadApp);
+window.addEventListener('load', loadApp, { once: true }); // Ensure the app loads only once when the window is fully loaded
