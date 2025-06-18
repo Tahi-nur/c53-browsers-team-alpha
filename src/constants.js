3fa1ab40f63a1cd79c2a4bf9ced88460d5a75ef2
hussam <hussam.it.altarsha@gmail.com>
2025-06-19 00:18:46 +0200

    chore: config updates

--- src/constants.js
+++ src/constants.js
@@ -8,4 +8,7 @@
 export const USER_INTERFACE_ID = 'user-interface';
 
 export const ANSWERS_LIST_ID = 'answers-list';
+export const SKIP_QUESTION_BUTTON_ID='skip-question-button';
+export const NEXT_QUESTION_BUTTON_ID='next-question-button';
+export const START_QUIZ_BUTTON_ID='start-quiz-button';
 
