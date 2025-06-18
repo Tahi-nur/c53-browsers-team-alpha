4d6816b98e0bbbe026797a171c9477a745792d6e
888i88 <igor_palun@icloud.com>
2025-06-18 14:38:43 +0200

    added theme-toggle

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
 const loadApp = () => {
-    initWelcomePage();
-};
+  initWelcomePage();
 
+  //switch toggle for development
+  const themeToggle = document.getElementById('theme-toggle');
+  if (themeToggle) {
+    themeToggle.addEventListener('click', () => {
+      document.body.classList.toggle('dark-theme');
+    });
+  }
+};
 window.addEventListener('load', loadApp, { once: true }); // Initialize app on load only once
